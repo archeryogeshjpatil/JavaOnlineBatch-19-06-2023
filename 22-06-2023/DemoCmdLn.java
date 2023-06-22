e747ea5bfab6cdd7c26af185844451d3e39c4298
Yogesh J Patil <yogeshjpatil@gmail.com>
2023-06-22 22:18:56 +0530

    cmdln

---
+++ 22-06-2023/DemoCmdLn.java
@@ -0,0 +1,12 @@
+class DemoCmdLn
+{
+	public static void main(String []args)
+	{
+		System.out.print("\n Demo Command Line Arguments");
+		System.out.println("\n First String:"+args[0]);
+		System.out.println(" Second String:"+args[1]);
+		System.out.print(" Third String:"+args[2]);
+	}
+}
+
+	 // println() is like printf() in C prog. ends with newline character('\n')
